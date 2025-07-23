f5772771328dc6a5f9f15f50ac16bf09cb9b4aad
Racheal Ochalek <lenig@rownd.io>
2025-07-23 11:46:25 -0500

    remove unsused import

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 from typing import List, Optional, Dict, Any
 from datetime import datetime
-from typing import Union
 
 class QueryRequest(BaseModel):
     question: str
